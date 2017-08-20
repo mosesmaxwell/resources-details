6019110667dc0932cd27c0869db22322364ebcc4
DESKTOP-MACDSH7\maxwell <mosesmaxwell@gmail.com>
2017-08-20 10:48:15 +0530

    title changes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title = "WebUI";
-  location = "chennai";
+  title = 'Team WebUI';
+  location = 'Chennai';
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit() {
